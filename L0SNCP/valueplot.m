04c0dadb42f07cb1e1ef50ed0dba6fbdfc07df8c
WeifengYang <85056442+Weifeng-Yang@users.noreply.github.com>
2026-03-24 11:45:44 +0800

    Update valueplot.m

--- L0SNCP/valueplot.m
+++ L0SNCP/valueplot.m
@@ -144,6 +144,8 @@ lossdata=losssum{s};
     mes='IBPL'; 
     elseif(trigger(s)==9)
     mes='IBPL$^{+}$';  
+    elseif(trigger(s)==10)
+    mes='IBPL$^{+}$-TP'; 
     end
     ls=length(mess);
     mess{ls+1}=mes;
@@ -195,6 +197,8 @@ for s=1:length(ind)
     mes='IBPL'; 
     elseif(trigger(k)==9)
     mes='IBPL$^{+}$'; 
+    elseif(trigger(s)==10)
+    mes='IBPL$^{+}$-TP'; 
     end
     ls=length(mess);
     mess{ls+1}=mes;
