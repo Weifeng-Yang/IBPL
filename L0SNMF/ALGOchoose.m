f9bd1a0834dba5ab7408e819c8869e940728be66
WeifengYang <85056442+Weifeng-Yang@users.noreply.github.com>
2026-03-24 11:46:20 +0800

    Update ALGOchoose.m

--- L0SNMF/ALGOchoose.m
+++ L0SNMF/ALGOchoose.m
@@ -91,7 +91,7 @@ btss=bts;
 atss=ats;
 
 elseif(flag==10)
-[vars,loss,tr,ats,bts]=IBPLtp(var,ngmar,aa,maxiteropt,at,bt,1,stopindex,r,t,btmax,rho); 
+[vars,loss,tr,ats,bts]=IBPLtp(var,ngmar,aa,maxiteropt,at,bt,1,stopindex,r,t,btmax,rho); %%IBPL+-TP
 varss=vars;
 lossdata=loss;
 trdata=tr;
