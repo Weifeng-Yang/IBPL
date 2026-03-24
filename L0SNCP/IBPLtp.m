64bba954caa0342990fd1dde2e4775eb1ee323bf
WeifengYang <85056442+Weifeng-Yang@users.noreply.github.com>
2026-03-24 11:44:03 +0800

    Add files via upload

---
+++ L0SNCP/IBPLtp.m
@@ -0,0 +1,119 @@
+%%  All parameters of this function are explained the same as 'main_Run_me' and 'ALGOchoose' functions
+function [var,loss,timerun,ats,bts]=IBPLtp(var,ngmar,aa,maxiteropt,at,bt,obj,stopindex,r,tk,btmax,rho)
+%% initialization algorithm
+loss=[];
+timerun=[0];
+btmaxtemp=btmax;
+tks=1;
+num=length(var);
+N=size(var{1},2);
+for i=1:num
+    varze{i}=zeros(size(ngmar,i),N);
+end
+if(obj==2)
+    tks=(1+sqrt(1+4*tks^2))/2;
+    bt=(tks-1)/(tks);
+    gamma=1.05;
+else
+    gamma=1.05;
+end
+varK=var;
+btmax=inf;
+
+
+
+
+at=min(gamma*bt,btmax);
+bts=[];
+ats=[];
+
+
+returnloss=norm(ngmar);
+loss(1)=compute(var,ngmar);
+t1=clock;
+
+
+for i=1:maxiteropt
+%% update parameters
+fprintf("%d\n",i);
+for j=1:num
+    varz{j}=var{j}+at*(var{j}-varK{j});
+    vv{j}=var{j}+bt*(var{j}-varK{j});
+end
+varK=var;
+var=IBPLupdate(vv,varz,varK,varze,num,ngmar,aa,r);
+loss(i+1)=compute(var,ngmar);
+
+
+%% Judging whether to extrapolate
+checkside=IBPLjudge(vv,varz,var,loss(i+1),loss(i),rho);
+if(checkside==1)
+    var=varK;
+    var=IBPLupdate(var,var,varK,varze,num,ngmar,aa,r);
+    loss(i+1)=compute(var,ngmar);
+end
+
+if(obj==1 && checkside==1)
+    bt=bt/tk; 
+elseif (obj==1 && checkside==0)
+    bt=min(bt*tk,btmax);
+end
+
+
+if(obj==2)
+    tks=(1+sqrt(1+4*tks^2))/2;
+    bt=(tks-1)/(tks);
+end
+
+check=abs(sqrt(loss(i+1)*2)-sqrt(loss(i)*2))/returnloss;
+if(check<1e-2)
+    btmax=btmaxtemp;
+end
+
+at=min(gamma*bt,btmax);
+bts(i)=bt;
+ats(i)=at;
+
+%% Check if termination condition is met
+fprintf("IBPLtp\n");
+for j=1:num
+    fprintf("nonzero:%d\n",nnz(var{j}~=0));
+end
+t2=clock;
+timerun(i+1)=etime(t2,t1);
+ReeK=loss(i)/returnloss;
+Ree=loss(i+1)/returnloss;
+Res=abs(Ree-ReeK);
+fprintf("Rel：%d\n",Res);
+stop=stopcheck(timerun,stopindex);
+if(stop==1)
+    fprintf("Number of terminations：%d\n",i);
+    pause(4);
+    break;
+end
+end
+end
+
+
+function checkside=IBPLjudge(vv,varz,var,lossplus,loss,rho)
+sumnorm=0;
+checkside=0;
+for i=1:length(var)
+    sumnorm=sumnorm+norm(vv{i}-var{i},'fro')^2+norm(varz{i}-var{i},'fro')^2;
+end
+if(lossplus>loss-rho/2*sumnorm)
+      checkside=1;
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+
